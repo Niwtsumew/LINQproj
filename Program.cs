664b743336a01b96d16d32e98eb3d80994368767
Bohdan Kotliarov <kotlyarovbog@gmail.com>
2023-12-18 00:30:55 +0100

    Query9

--- Program.cs
+++ Program.cs
@@ -22,7 +22,7 @@ namespace Practice_Linq
             Query6(games);
             Query7(games);
             Query8(games);
-            //Query9(games);
+            Query9(games);
             //Query10(games);
 
         }
@@ -241,13 +241,35 @@ namespace Practice_Linq
             //Query 9: Перетворити всі матчі UEFA Nations League у 2023 році на матчі з наступними властивостями:
             // MatchYear - рік матчу, Game - назви обох команд через дефіс (першою - Home_team), Result - результат для першої команди (Win, Loss, Draw)
 
-            var selectedGames = games;   // Корегуємо запит !!!
+            var selectedGames = games
+                    .Where(game =>
+                        game.Tournament.Equals("UEFA Nations League") &&
+                        game.Date.Year == 2023)
+                    .Select(game => new
+                    {
+                        MatchYear = game.Date.Year,
+                        Game = $"{game.Home_team}-{game.Away_team}",
+                        Result = GetResult(game.Home_score, game.Away_score),
+                    }).ToList();   // Корегуємо запит !!!
+
+            static string GetResult(int homeScore, int awayScore)
+            {
+                if (homeScore > awayScore)
+                    return "Win";
+                else if (homeScore < awayScore)
+                    return "Loss";
+                else
+                    return "Draw";
+            }
 
             // Перевірка
             Console.WriteLine("\n======================== QUERY 9 ========================");
 
             // див. приклад як має бути виведено:
-
+            foreach (var game in selectedGames)
+            {
+                Console.WriteLine($"{game.MatchYear} {game.Game}, Result for team1: {game.Result}");
+            }
 
         }
 
